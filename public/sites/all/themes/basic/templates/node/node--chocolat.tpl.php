d78715b4459efa49fe2381c40bde96d9fe3a32c0
Zouille <arfaouiham@eisti.eu>
2015-10-25 17:48:59 +0100

    Ajout de devel et de choco.tpl

---
+++ public/sites/all/themes/basic/templates/node/node--chocolat.tpl.php
@@ -0,0 +1,21 @@
+<article id="node-chocolat">
+	
+	<?php if(!empty($title)):?>
+		<h1 class="node-title"><?php print $titre?></h1>
+	<?php endif;?>
+	<br>
+	<?php if(!empty($image)):?>
+		<img class="node-img" src=<?php print $image;?>>
+	<?php endif;?>
+	<?php if(!empty($prix_entreprise) && !empty($prix_particulier) && !empty($poids) ):?>
+		<ul style="list-style-type:none">
+			<li class="prix-particulier"><?php print $prix_particulier;?></li>
+			<li class="prix-entreprise"><?php print $prix_entreprise;?></li>
+			<li class="poids"><?php print $poids;?></li>
+		</ul>
+	<?php endif;?>
+	<?php if(!empty($description)):?>
+		<p class="chocolat-description"><?php print $description;?></p>
+	<?php endif;?>
+	
+</article>
